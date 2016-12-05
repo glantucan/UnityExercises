19c11fac3642b99312bf90c1466998b0aafb3998
glantucan <glantucan@gmail.com>
2016-12-05 08:41:53 +0100

    Añadido machineGun

---
+++ machineGun/Assets/Scripts/Gun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	[SerializeField] GameObject target;
+	[SerializeField] GameObject bulletPrefab;
+	[SerializeField] float v;
+
+	private GameObject bulletInstance;
+	private Vector3 direction;
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.frameCount % 10 == 0) {
+			bulletInstance = Object.Instantiate(bulletPrefab);
+			bulletInstance.transform.position = this.transform.position + this.transform.forward * 1.5F + this.transform.up; 
+
+			Vector3 distance = target.transform.position - bulletInstance.transform.position;
+			direction = distance.normalized;
+		}
+
+		if (bulletInstance != null) {
+			bulletInstance.transform.position += v * direction*Time.deltaTime; 	
+		}
+	}
+}
